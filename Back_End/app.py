0aaee8430708b8c6789a73ecedc1e85ad73f136e
Heisn Nithysingha <heisn.nithy@gmail.com>
2024-01-11 13:54:39 -0500

    Got Backend to integrate chessboardjs on a low level
    Got the backend to move a peice on the board

--- Back_End/app.py
+++ Back_End/app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 CORS(app)
 @app.route('/')
 def home():
-   return "Success!!!!!!!!"
+   return "d1-d6"
 if __name__ == '__main__':
    app.run()
--- Front_End/js/main.js
+++ Front_End/js/main.js
@@ -6,13 +6,18 @@ var board1 = Chessboard('board1', {
 })
   $('#startBtn').on('click', board1.start)
   $('#clearBtn').on('click', board1.clear)
+
+let test = null;
+fetch('http://127.0.0.1:5000/')// Fetch request
+ .then(response => response.text())
+ .then(data => {      //Stores data into variable to then be used
+ test = data;         //After initialising variable to then be used in button to move peice
+ $('#Test').on('click', function() {
+   board1.move(test); //Moves peice according to back end
+ });
+})
+ .catch(error => console.error('Error:', error)); // Catch any errors
+
 }, 0);
-
-fetch('http://127.0.0.1:5000/', {
-}) .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-
-
 
 
